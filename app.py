13c0206629dadbbd85d97637004a357ade2dd22b
Siva Sankaran R <107678884+SivaSankaran16@users.noreply.github.com>
2024-06-28 16:36:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,149 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from module import display
+
+load_dotenv()
+st.session_state.api_key = os.environ.get("OPEN_AI_KEY") 
+
+if "page" not in st.session_state:
+    st.session_state.page = "home"
+if "conversation" not in st.session_state:
+    st.session_state.conversation = []
+if "uploaded_file" not in st.session_state:
+    st.session_state.uploaded_file = None
+if "text" not in st.session_state:
+    st.session_state.text = " "
+if "show_clicked" not in st.session_state:
+    st.session_state.show_clicked = False
+
+if st.session_state.page == "home":
+    st.header("Patient Lab Report Analyzer")
+
+    try:
+        st.session_state.uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+        if st.session_state.uploaded_file:
+            if not st.session_state.show_clicked:
+                if st.button("ANALYZE") :
+                    try:
+                        dis = display(st.session_state.uploaded_file, st.session_state.api_key)
+                        try:
+                            t_im=dis.request()
+                            st.session_state.text = t_im
+                        except:
+                            pass
+                        text = dis.analyze(st.session_state.text)
+                        st.session_state.text = text
+                        st.title("REPORT")
+                        st.write(st.session_state.text)
+                        st.title("AI GENERATED SUMMARY")
+                        sum=dis.summary(st.session_state.text)
+                        st.write(sum)
+                        st.session_state.show_clicked = True
+                    except Exception as e:
+                        st.error(f"Error processing PDF: {e}")
+            if st.session_state.show_clicked and st.session_state.page != "chatbot":
+                if st.button("CHAT_BOT"):
+                    st.session_state.page = "chatbot"
+
+    except :
+        pass
+
+elif st.session_state.page == "chatbot":
+    if "st.session_state.user_input" not in st.session_state:
+        st.session_state.user_input = None
+    st.session_state.user_input = st.text_input("You:", key="input")
+
+    if st.button("Send"):
+        if st.session_state.user_input:
+            dis = display(st.session_state.uploaded_file, st.session_state.api_key)
+            m = dis.get_openai_response(st.session_state.text, st.session_state.user_input)
+            bot_response = m
+            st.session_state.conversation.append({"role": "assistant", "content": bot_response})
+            st.session_state.conversation.append({"role": "user", "content": st.session_state.user_input})
+
+
+    for i in range(len(st.session_state.conversation)):
+        message=st.session_state.conversation[len(st.session_state.conversation)-i-1]
+        if message["role"] == "assistant":
+            
+            
+            st.write(f"**bot:** {message['content']}")
+            
+        else:
+            st.write("                   ____________________________________________________________________________________________________________________________________________                                ")
+            st.write(f"**user:** {message['content']}")
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if st.session_state.bot:
+#         # @st.cache_resource
+#     def initialize():
+#         chat= chat_gen(st.session_state.api_key,st.session_state.uploaded_file)
+#         return chat
+
+#     st.session_state.chat=initialize()
+
+#     st.title("Doc chat Bot")
+
+#     # Initialize chat history
+#     if "messages" not in st.session_state:
+#         st.session_state.messages = []
+
+#     # Display chat messages from history on app rerun
+#     for message in st.session_state.messages:
+#         with st.chat_message(message["role"]):
+#             st.markdown(message["content"])
+
+#     # React to user input
+#     if prompt := st.chat_input("What is up?"):
+#         # Display user message in chat message container
+#         st.chat_message("user").markdown(prompt)
+#         # Add user message to chat history
+#         st.session_state.messages.append({"role": "user", "content": prompt})
+
+#         response = st.session_state.chat.ask_pdf(prompt,) 
+#         #f"Echo: {prompt}"
+#         # Display assistant response in chat message container
+#         with st.chat_message("assistant"):
+#             st.markdown(response)
+#         # Add assistant response to chat history
+#         st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+
---
+++ module.py
@@ -0,0 +1,157 @@
+
+from langchain.text_splitter import CharacterTextSplitter
+from PyPDF2 import PdfReader
+from openai import OpenAI 
+import fitz
+import PIL.Image
+import io
+import base64
+import os
+
+class pdf_extractor():
+    def __init__(self,pdf_path) -> None:
+        self.pdf=pdf_path
+    def extract(self):
+        pdf=PdfReader(self.pdf)
+        raw_text=""
+        for i,page in enumerate(pdf.pages):
+            content = page.extract_text()
+            if content:
+                raw_text+=content
+                raw_text+=content
+        text_splitter=CharacterTextSplitter(
+            separator='\n',
+            chunk_size=800,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        text=text_splitter.split_text(raw_text)
+        return text
+
+
+class display:
+    def __init__(self, pdf,key):
+        self.key = key
+        self.pdf = pdf
+    
+    def summary(self,text):
+        client = OpenAI(api_key=self.key)
+        message = [
+                    {"role": "system", "content": "You are a data analyst."},
+                    {"role": "user", "content": f"Analyze the following data and provide 3-4 line of summary: {text}"}
+                ]
+
+
+        response =  client.chat.completions.create(
+                    model="gpt-4o",
+                messages=message)
+            
+        return response.choices[0].message.content
+
+
+
+    def analyze(self,im_text):
+        client = OpenAI(api_key=self.key)
+        extractor =pdf_extractor(self.pdf)
+        text = extractor.extract()
+        text+=im_text
+        if len(text)==0:
+            return ""
+        else:
+            message = [
+                {"role": "system", "content": "You are a medical report analyzer."},
+                {"role": "user", "content": f" your are given input of medical reports extracted from the pdf and information extracted from images,analyze both and extarct detailed info and don't add the summary: {text}"}
+            ]
+            response =  client.chat.completions.create(
+                    model="gpt-4o",
+                messages=message)
+            text=response.choices[0].message.content
+            out="output.pdf"
+            # self.text_to_pdf(text,"output.pdf")
+            return text
+    def get_openai_response(self,info,query):
+        client = OpenAI(api_key=self.key)
+        message = [
+        {"role": "system", "content": "You are a data analyst."},
+        {"role": "user", "content": f"Analyze the following information and answer the query: {info}\n\nUser Query: {query}"}
+    ]
+  
+        response = client.chat.completions.create(
+            model="gpt-4o", 
+            messages=message
+        )
+        return response.choices[0].message.content
+    def clear_output_folder(self,output_folder):
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        else:
+            for file_name in os.listdir(output_folder):
+                file_path = os.path.join(output_folder, file_name)
+                try:
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+    def extract_image(self):
+        output_folder="output_folder"
+        self.clear_output_folder(output_folder)
+        pdf = fitz.open(self.pdf)
+        image_paths = []
+
+        for page_num in range(len(pdf)):
+            page = pdf.load_page(page_num)
+            images = page.get_images()
+
+            for index, image in enumerate(images):
+                base_img = pdf.extract_image(image[0])
+                image_data = base_img["image"]
+                img = PIL.Image.open(io.BytesIO(image_data))
+
+                extension = base_img["ext"]
+                image_filename = f"image{page_num + 1}_{index + 1}.{extension}"
+                image_path = os.path.join(output_folder, image_filename)
+                img.save(open(image_path, "wb"))
+
+                image_paths.append(image_path)
+        return image_paths
+    
+    def encode_image_to_base64(self,image_path):
+        with open(image_path, 'rb') as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    def message(self):
+        images=self.extract_image()
+        
+        messages=[]
+        for image in images:
+            base64_image=self.encode_image_to_base64(image_path=image)
+            mess={
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "your are given image of medical reports analyze and extarct detailed info"},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            }
+            messages.append(mess)
+        return messages
+        
+    def request(self):
+        client = OpenAI(api_key=self.key)
+        messages=self.message()
+        response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages)
+        return (response.choices[0].message.content)
+
+
+        
+
+
+        
+
+
