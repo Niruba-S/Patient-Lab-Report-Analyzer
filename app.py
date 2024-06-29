571cceb9b922e21483ce5896d05f88b64370aa36
Siva Sankaran R <107678884+SivaSankaran16@users.noreply.github.com>
2024-06-29 12:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+
 import streamlit as st
 import os
 from dotenv import load_dotenv
 from module import display
 
-# def main():
 load_dotenv()
 st.session_state.api_key = os.environ.get("OPEN_AI_KEY") 
+
 
 if "page" not in st.session_state:
     st.session_state.page = "home"
@@ -15,71 +16,105 @@ if "uploaded_file" not in st.session_state:
     st.session_state.uploaded_file = None
 if "text" not in st.session_state:
     st.session_state.text = " "
-if "show_clicked" not in st.session_state:
-    st.session_state.show_clicked = False
+
 if "Image_text" not in st.session_state:
     st.session_state.Image_text = ""
+if "sum" not in st.session_state:
+    st.session_state.sum = ""
 
-if st.session_state.page == "home":
-    st.header("Patient Lab Report Analyzer")
+st.header("Patient Lab Report Analyzer")
 
-    try:
+try:
+    if "show_clicked" not in st.session_state:
+        st.session_state.show_clicked = False
+    if not st.session_state.show_clicked:
         st.session_state.uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
         if st.session_state.uploaded_file:
-            if not st.session_state.show_clicked:
-                if st.button("ANALYZE") :
+            if st.button("ANALYZE") :
+                with st.spinner("ANALYZING"):
                     try:
                         dis = display(st.session_state.uploaded_file, st.session_state.api_key)
                         try:
-                            t_im=dis.request()
+                            t_im = dis.request()
                             st.session_state.text = t_im
-                            st.session_state.Image_text =t_im
+                            st.session_state.Image_text = t_im
                         except:
                             pass
                         text = dis.analyze(st.session_state.text)
                         st.session_state.text = text
                         st.title("REPORT")
                         st.write(st.session_state.text)
-                        
-                        st.title("AI GENERATED SUMMARY")
 
-                        sum=dis.summary(st.session_state.text)
-                        
-                        st.write(sum)
-                        st.session_state.text+=st.session_state.Image_text
+                        if len(st.session_state.text) > 0:
+                            st.title("AI GENERATED SUMMARY")
+                            st.session_state.sum = dis.summary(st.session_state.text)
+                            st.write(st.session_state.sum)
+                        st.session_state.text += st.session_state.Image_text
                         st.session_state.show_clicked = True
                     except Exception as e:
                         st.error(f"Error processing PDF: {e}")
-            if st.session_state.show_clicked and st.session_state.page != "chatbot":
-                if st.button("CHAT_BOT"):
-                    st.session_state.page = "chatbot"
-    except :
-        pass
+                st.button("chat")
+                
 
-elif st.session_state.page == "chatbot":
-    if "st.session_state.user_input" not in st.session_state:
-        st.session_state.user_input = None
+    elif st.session_state.show_clicked:
+        print(st.session_state.show_clicked)
+        st.write(st.session_state.sum)
+        if "user_input" not in st.session_state:
+            st.session_state.user_input = None
         st.session_state.user_input = st.text_input("You:", key="input")
 
-    if st.button("Send"):
-        if st.session_state.user_input:
-            dis = display(st.session_state.uploaded_file, st.session_state.api_key)
-            m = dis.get_openai_response(st.session_state.text, st.session_state.user_input)
-            bot_response = m
-            st.session_state.conversation.append({"role": "assistant", "content": bot_response})
-            st.session_state.conversation.append({"role": "user", "content": st.session_state.user_input})
+        if st.button("Send"):
+            if st.session_state.user_input:
+                dis = display(st.session_state.uploaded_file, st.session_state.api_key)
+                m = dis.get_openai_response(st.session_state.text, st.session_state.user_input)
+                bot_response = m
+                st.session_state.conversation.append({"role": "assistant", "content": bot_response})
+                st.session_state.conversation.append({"role": "user", "content": st.session_state.user_input})
+
+        for i in range(len(st.session_state.conversation)):
+            message = st.session_state.conversation[len(st.session_state.conversation) - i - 1]
+            if message["role"] == "assistant":
+                st.write(f"**bot:** {message['content']}")
+            else:
+                st.write("__________________________________________________________________________________________________________________________________________")
+                st.write(f"**user:** {message['content']}")
+
+except Exception as e:
+    st.error(f"Unexpected error: {e}")
+
+            
 
 
-    for i in range(len(st.session_state.conversation)):
-        message=st.session_state.conversation[len(st.session_state.conversation)-i-1]
-        if message["role"] == "assistant":
-            
-            
-            st.write(f"**bot:** {message['content']}")
-            
-        else:
-            st.write("                   ________________________________________________________________________________________________                                ")
-            st.write(f"**user:** {message['content']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
